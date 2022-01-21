f31ba3bb8d6e03673e829116195e08e786bca4d7
antoinerr <ant.romero2@orange.fr>
2022-01-21 15:17:51 +0000

    Add the raytracer project

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: `gatsby-plugin-react-svg`,
       options: {
         rule: {
           include: /assets/
